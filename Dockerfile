9d20958e6a04ba265dcfc575c52922a710ff3b3f
Hendrik Voortman <hendrik.voortman@gmail.com>
2020-01-01 15:38:51 +0000

    adding docker startup params + 10 sec delay loop

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@ FROM openjdk:11
 COPY . /var/www/java  
 COPY target/classes /var/www/java  
 WORKDIR /var/www/java  
+#ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Dxyz=1234" ]
+ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Dxyz=1234 \"com.handf.dockertest.DockerTest\" \"AA\" 1234 'X' 1 2 3 4 5" ]
+#ENTRYPOINT [ "sh", "-c", "java \"com.handf.dockertest.DockerTest\" \"AA\" 1234 'X' " ]
 RUN javac src/main/java/com/handf/dockertest/DockerTest.java  
 CMD ["java", "com.handf.dockertest.DockerTest"]  
--- src/main/java/com/handf/dockertest/DockerTest.java
+++ src/main/java/com/handf/dockertest/DockerTest.java
@@ -4,15 +4,22 @@ import java.util.Date;
 
 public class DockerTest {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) throws InterruptedException {		
 		Date d = new Date();
 		StringBuilder builder = new StringBuilder();
 		builder
-			.append("******* ").append(d.toString()).append(" [v1] *******")
-			.append("\nargs:");
-			for (String arg : args) {
-				builder.append("\n").append(builder.append(arg));
-			}			
+		.append("******* ").append(d.toString()).append(" [v1] *******")
+		.append("\nargs:");
+		for (String arg : args) {
+			builder.append("\n").append(builder.append(arg));
+		}			
 		System.out.println(builder);
+
+		final int SLEEP_DURATION = 10;
+		for (int i=1; i <= SLEEP_DURATION; i++) {
+			System.out.println("delay=" + i);
+			Thread.sleep(1000);
+		}
+		System.out.println("finished sleeping...");			
 	}
 }
