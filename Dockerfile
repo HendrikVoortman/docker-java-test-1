7babfb9332163ea2b9335a0bac82b9d182271344
Hendrik Voortman <hendrik.voortman@gmail.com>
2020-01-01 13:36:11 +0000

    changing package name and updating git ignore

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM openjdk:11
 COPY . /var/www/java  
 COPY target/classes /var/www/java  
 WORKDIR /var/www/java  
-RUN javac src/main/java/dockertest/DockerTest.java  
-CMD ["java", "dockertest.DockerTest"]  
+RUN javac src/main/java/com/handf/dockertest/DockerTest.java  
+CMD ["java", "com.handf.dockertest.DockerTest"]  
--- src/main/java/com/handf/dockertest/DockerTest.java
+++ src/main/java/com/handf/dockertest/DockerTest.java
@@ -1,4 +1,4 @@
-package dockertest;
+package com.handf.dockertest;
 
 import java.util.Date;
 
@@ -8,7 +8,7 @@ public class DockerTest {
 		Date d = new Date();
 		StringBuilder builder = new StringBuilder();
 		builder
-			.append("******* ").append(d.toString()).append("*******")
+			.append("******* ").append(d.toString()).append(" [v1] *******")
 			.append("\nargs:");
 			for (String arg : args) {
 				builder.append("\n").append(builder.append(arg));
